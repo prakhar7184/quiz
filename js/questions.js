ba28b4856d7d7981936f0b49f7c51292f01cca6a
mymoviesfree <100144322+mymoviesfree@users.noreply.github.com>
2022-02-23 13:46:59 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,446 @@
+// creating an array and passing the number, questions, options, and answers
+var count=0;
+var a=['+','-','*','/'];
+var num1=Math.floor(Math.random() * 10) + 1;
+var num2=Math.floor(Math.random() * 10) + 1;
+var sign1=a[Math.floor(Math.random() * 4)];
+var q1=`${num1}${sign1}${num2}=`;
+if(sign1=='+'){var result1=num1+num2;}
+else if(sign1=='-'){var result1=num1-num2;}
+else if(sign1=='*'){var result1=num1*num2;}
+else if(sign1=='/'){var result1=num1/num2;}
+
+var num3=Math.floor(Math.random() * 10) + 1;
+var num4=Math.floor(Math.random() * 10) + 1;
+var sign2=a[Math.floor(Math.random() * 4)];
+var q2=`${num3}${sign2}${num4}=`;
+if(sign2=='+'){var result2=num3+num4;}
+else if(sign2=='-'){var result2=num3-num4;}
+else if(sign2=='*'){var result2=num3*num4;}
+else if(sign2=='/'){var result2=num3/num4;}
+
+var num5=Math.floor(Math.random() * 10) + 1;
+var num6=Math.floor(Math.random() * 10) + 1;
+var sign3=a[Math.floor(Math.random() * 4)];
+var q3=`${num5}${sign3}${num6}=`;
+if(sign3=='+'){var result3=num5+num6;}
+else if(sign3=='-'){var result3=num5-num6;}
+else if(sign3=='*'){var result3=num5*num6;}
+else if(sign3=='/'){var result3=num5/num6;}
+
+var num7=Math.floor(Math.random() * 10) + 1;
+var num8=Math.floor(Math.random() * 10) + 1;
+var sign4=a[Math.floor(Math.random() * 4)];
+var q4=`${num7}${sign4}${num8}=`;
+if(sign4=='+'){var result4=num7+num8;}
+else if(sign4=='-'){var result4=num7-num8;}
+else if(sign4=='*'){var result4=num7*num8;}
+else if(sign4=='/'){var result4=num7/num8;}
+
+var num9=Math.floor(Math.random() * 10) + 1;
+var num10=Math.floor(Math.random() * 10) + 1;
+var sign5=a[Math.floor(Math.random() * 4)];
+var q5=`${num9}${sign5}${num10}=`;
+if(sign5=='+'){var result5=num9+num10;}
+else if(sign5=='-'){var result5=num9-num10;}
+else if(sign5=='*'){var result5=num9*num10;}
+else if(sign5=='/'){var result5=num9/num10;}
+
+var num11=Math.floor(Math.random() * 10) + 1;
+var num12=Math.floor(Math.random() * 10) + 1;
+var sign6=a[Math.floor(Math.random() * 4)];
+var q6=`${num11}${sign6}${num12}=`;
+if(sign6=='+'){var result6=num11+num12;}
+else if(sign6=='-'){var result6=num11-num12;}
+else if(sign6=='*'){var result6=num11*num12;}
+else if(sign6=='/'){var result6=num11/num12;}
+
+var num13=Math.floor(Math.random() * 10) + 1;
+var num14=Math.floor(Math.random() * 10) + 1;
+var sign7=a[Math.floor(Math.random() * 4)];
+var q7=`${num13}${sign7}${num14}=`;
+if(sign7=='+'){var result7=num13+num14;}
+else if(sign7=='-'){var result7=num13-num14;}
+else if(sign7=='*'){var result7=num13*num14;}
+else if(sign7=='/'){var result7=num13/num14;}
+
+var num15=Math.floor(Math.random() * 10) + 1;
+var num16=Math.floor(Math.random() * 10) + 1;
+var sign8=a[Math.floor(Math.random() * 4)];
+var q8=`${num15}${sign8}${num16}=`;
+if(sign8=='+'){var result8=num15+num16;}
+else if(sign8=='-'){var result8=num15-num16;}
+else if(sign8=='*'){var result8=num15*num16;}
+else if(sign8=='/'){var result8=num15/num16;}
+
+var num17=Math.floor(Math.random() * 10) + 1;
+var num18=Math.floor(Math.random() * 10) + 1;
+var sign9=a[Math.floor(Math.random() * 4)];
+var q9=`${num17}${sign9}${num18}=`;
+if(sign9=='+'){var result9=num17+num18;}
+else if(sign9=='-'){var result9=num17-num18;}
+else if(sign9=='*'){var result9=num17*num18;}
+else if(sign9=='/'){var result9=num17/num18;}
+
+var num19=Math.floor(Math.random() * 10) + 1;
+var num20=Math.floor(Math.random() * 10) + 1;
+var sign10=a[Math.floor(Math.random() * 4)];
+var q10=`${num19}${sign10}${num20}=`;
+if(sign10=='+'){var result10=num10+num20;}
+else if(sign10=='-'){var result10=num10-num20;}
+else if(sign10=='*'){var result10=num10*num20;}
+else if(sign10=='/'){var result10=num10/num20;}
+
+var num21=Math.floor(Math.random() * 10) + 1;
+var num22=Math.floor(Math.random() * 10) + 1;
+var sign11=a[Math.floor(Math.random() * 4)];
+var q11=`${num21}${sign11}${num22}=`;
+if(sign11=='+'){var result11=num21+num22;}
+else if(sign11=='-'){var result11=num21-num22;}
+else if(sign11=='*'){var result11=num21*num22;}
+else if(sign11=='/'){var result11=num21/num22;}
+
+var num23=Math.floor(Math.random() * 10) + 1;
+var num24=Math.floor(Math.random() * 10) + 1;
+var sign12=a[Math.floor(Math.random() * 4)];
+var q12=`${num23}${sign12}${num24}=`;
+if(sign12=='+'){var result12=num23+num24;}
+else if(sign12=='-'){var result12=num23-num24;}
+else if(sign12=='*'){var result12=num23*num24;}
+else if(sign12=='/'){var result12=num23/num24;}
+
+var num25=Math.floor(Math.random() * 10) + 1;
+var num26=Math.floor(Math.random() * 10) + 1;
+var sign13=a[Math.floor(Math.random() * 4)];
+var q13=`${num25}${sign13}${num26}=`;
+if(sign13=='+'){var result13=num25+num26;}
+else if(sign13=='-'){var result13=num25-num26;}
+else if(sign13=='*'){var result13=num25*num26;}
+else if(sign13=='/'){var result13=num25/num26;}
+
+var num27=Math.floor(Math.random() * 10) + 1;
+var num28=Math.floor(Math.random() * 10) + 1;
+var sign14=a[Math.floor(Math.random() * 4)];
+var q14=`${num27}${sign14}${num28}=`;
+if(sign14=='+'){var result14=num27+num28;}
+else if(sign14=='-'){var result14=num27-num28;}
+else if(sign14=='*'){var result14=num27*num28;}
+else if(sign14=='/'){var result14=num27/num28;}
+
+var num29=Math.floor(Math.random() * 10) + 1;
+var num30=Math.floor(Math.random() * 10) + 1;
+var sign15=a[Math.floor(Math.random() * 4)];
+var q15=`${num29}${sign15}${num30}=`;
+if(sign15=='+'){var result15=num29+num30;}
+else if(sign15=='-'){var result15=num29-num30;}
+else if(sign15=='*'){var result15=num29*num30;}
+else if(sign15=='/'){var result15=num29/num30;}
+
+var num31=Math.floor(Math.random() * 10) + 1;
+var num32=Math.floor(Math.random() * 10) + 1;
+var sign16=a[Math.floor(Math.random() * 4)];
+var q16=`${num31}${sign16}${num32}=`;
+if(sign16=='+'){var result16=num31+num32;}
+else if(sign16=='-'){var result16=num31-num32;}
+else if(sign16=='*'){var result16=num31*num32;}
+else if(sign16=='/'){var result16=num31/num32;}
+
+var num33=Math.floor(Math.random() * 10) + 1;
+var num34=Math.floor(Math.random() * 10) + 1;
+var sign17=a[Math.floor(Math.random() * 4)];
+var q17=`${num33}${sign17}${num34}=`;
+if(sign17=='+'){var result17=num33+num34;}
+else if(sign17=='-'){var result17=num33-num34;}
+else if(sign17=='*'){var result17=num33*num34;}
+else if(sign17=='/'){var result17=num33/num34;}
+
+var num35=Math.floor(Math.random() * 10) + 1;
+var num36=Math.floor(Math.random() * 10) + 1;
+var sign18=a[Math.floor(Math.random() * 4)];
+var q18=`${num35}${sign18}${num36}=`;
+if(sign18=='+'){var result18=num35+num36}
+else if(sign18=='-'){var result18=num35-num36}
+else if(sign18=='*'){var result18=num35*num36}
+else if(sign18=='/'){var result18=num35/num36}
+
+var num37=Math.floor(Math.random() * 10) + 1;
+var num38=Math.floor(Math.random() * 10) + 1;
+var sign19=a[Math.floor(Math.random() * 4)];
+var q19=`${num37}${sign19}${num38}=`;
+if(sign19=='+'){var result19=num37+num38;}
+else if(sign19=='-'){var result19=num37-num38;}
+else if(sign19=='*'){var result19=num37*num38;}
+else if(sign19=='/'){var result19=num37/num38;}
+
+var num39=Math.floor(Math.random() * 10) + 1;
+var num40=Math.floor(Math.random() * 10) + 1;
+var sign20=a[Math.floor(Math.random() * 4)];
+var q20=`${num39}${sign20}${num40}=`;
+if(sign20=='+'){var result20=num39+num40;}
+else if(sign20=='-'){var result20=num39-num40;}
+else if(sign20=='*'){var result20=num39*num40;}
+else if(sign20=='/'){var result20=num39/num40;}
+
+let questions = [
+    {
+    numb: ++count,
+    question: q1,
+    answer: result1,
+    options: [
+      result1 + 1,
+      result1,
+      result1 + 2,
+      result1 - 1
+    ]
+  },
+  {
+    numb: ++count,
+    question: q2,
+    answer: result2,
+    options: [
+      result2,
+      result2 + 1,
+      result2 + 2,
+      result2 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q3,
+    answer: result3,
+    options: [
+      result3 + 1,
+      result3 + 2,
+      result3,
+      result3 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q4,
+    answer: result4,
+    options: [
+      result4,
+      result4 + 1,
+      result4 + 2,
+      result4 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q5,
+    answer: result5,
+    options: [
+      result5 + 1,
+      result5,
+      result5 + 2,
+      result5 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q6,
+    answer: result6,
+    options: [
+      result6 + 1,
+      result6 + 2,
+      result6 - 1,
+      result6
+    ]
+  },{
+    numb: ++count,
+    question: q7,
+    answer: result7,
+    options: [
+      result7 + 1,
+      result7,
+      result7 + 2,
+      result7 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q8,
+    answer: result8,
+    options: [
+      result8 + 1,
+      result8 + 2,
+      result8,
+      result8 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q9,
+    answer: result9,
+    options: [
+      result9,
+      result9 + 1,      
+      result9 + 2,
+      result9 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q10,
+    answer: result10,
+    options: [
+      result10 + 1,
+      result10 + 2,
+      result10 - 1,
+      result10
+    ]
+  },{
+    numb: ++count,
+    question: q11,
+    answer: result11,
+    options: [
+      result11 + 1,
+      result11 + 2,
+      result11 - 1,
+      result11
+    ]
+  },{
+    numb: ++count,
+    question: q12,
+    answer: result12,
+    options: [
+      result12 + 1,
+      result12,
+      result12 + 2,
+      result12 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q13,
+    answer: result13,
+    options: [
+      result13 + 1,
+      result13 + 2,
+      result13,
+      result13 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q14,
+    answer: result14,
+    options: [
+      result14,
+      result14 + 1,
+      result14 + 2,
+      result14 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q15,
+    answer: result15,
+    options: [
+      result15 + 1,
+      result15,
+      result15 + 2,
+      result15 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q16,
+    answer: result16,
+    options: [
+      result16,
+      result16 + 1,
+      result16 + 2,
+      result16 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q17,
+    answer: result17,
+    options: [
+      result17 + 1,
+      result17 + 2,
+      result17 - 1,
+      result17
+    ]
+  },{
+    numb: ++count,
+    question: q18,
+    answer: result18,
+    options: [
+      result18 + 1,
+      result18,
+      result18 + 2,
+      result18 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q19,
+    answer: result19,
+    options: [
+      result19 + 1,
+      result19 + 2,
+      result19,
+      result19 - 1
+    ]
+  },{
+    numb: ++count,
+    question: q20,
+    answer: result20,
+    options: [
+      result20,
+      result20 + 1,
+      result20 + 2,
+      result20 - 1
+    ]
+  }
+
+  //   {
+  //   numb: 2,
+  //   question: "2+4=",
+  //   answer: "6",
+  //   options: [
+  //     "6",
+  //     "7",
+  //     "9",
+  //     "10"
+  //   ]
+  // },
+  //   {
+  //   numb: 3,
+  //   question: "23+2=",
+  //   answer: "25",
+  //   options: [
+  //     "24",
+  //     "23",
+  //     "25",
+  //     "26"
+  //   ]
+  // },
+  //   {
+  //   numb: 4,
+  //   question: "22+21=",
+  //   answer: "43",
+  //   options: [
+  //     "44",
+  //     "42",
+  //     "43",
+  //     "45"
+  //   ]
+  // },
+  //   {
+  //   numb: 5,
+  //   question: "5+5=",
+  //   answer: "10",
+  //   options: [
+  //     "11",
+  //     "12",
+  //     "13",
+  //     "10"
+  //   ]
+  // },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
